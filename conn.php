ccbe2fed1e86b5a1878272d1d5cc960261e3774a
LaraSV08 <140665251+LaraSV08@users.noreply.github.com>
2024-09-20 11:49:12 -0600

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,4 @@
 <?php
-
 class conexion{
 
     public $host = '127.0.0.1';
@@ -9,20 +8,19 @@ class conexion{
     public $port = '3306';
     public $charset = 'utf8mb4';
     public $options = [
-        \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_ERRMODE=>\PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES => false
     ];
 
     public function conectar(){
         try{
-            $pdo = new PDO("mysql:host=($this->host);dbname=($this->db);charset=($this->charset);port=($this->port)",$this->user,$this->pass,$this->options);
+            $pdo = new PDO("mysql:host={$this->host};dbname={$this->db};charset={$this->charset};port={$this->port}",$this->user,$this->pass,$this->options);
             return $pdo;
         }catch(PDOException $exp){
-            echo("HUBO UN ERROR EN LA CONEXIÓN".$exp->getMessage());
+            echo("HUBO UN ERROR EN LA CONEXION".$exp->getMessage());
         }
     }
 }
-
 
 ?>
